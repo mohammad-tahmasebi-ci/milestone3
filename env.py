0d61d124e72e1ab3768cdf1c7f09fe9989061402
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2025-03-09 00:30:34 +0000

    Create env.py
    Create env.py containing environment variables

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "Th3Ch33kyBr0wnF0xJmped0v3rTheW411")
+os.environ.setdefault("DEBUG", "True")
+os.environ.setdefault("DEVELOPMENT", "True")
+os.environ.setdefault("DB_URL", "postgresql://postgres:admin@localhost/appointments")
